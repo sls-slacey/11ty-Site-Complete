ebafeefd462d72085533b7ba3507a7c0f26c4b53
mysleekdesigns <simonlacey20@gmail.com>
2025-06-05 12:20:48 -0400

    Fix CSS and images: Add SCSS compilation and static asset passthrough - Configure @11tyrocks/eleventy-plugin-sass-lightningcss for SCSS compilation - Add passthrough copy for images, JS files, and favicon - Ensures CSS and images work correctly when cloning the repository

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,18 @@
+const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
+
 module.exports = function(eleventyConfig) {
+  // Add the SASS plugin to compile SCSS files
+  eleventyConfig.addPlugin(eleventySass);
+
+  // Copy images and other static assets to the output directory
+  eleventyConfig.addPassthroughCopy("src/images");
+  
+  // Copy any JavaScript files if they exist
+  eleventyConfig.addPassthroughCopy("src/js");
+  
+  // Copy favicon and other root assets
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+
   // Set the input and output directories
   return {
     dir: {
